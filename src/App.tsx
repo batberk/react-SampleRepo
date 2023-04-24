06252652e659298214ce106e81cdde0288198873
batberk <104142067+batberk@users.noreply.github.com>
2023-04-24 12:09:36 +0000

    xx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect , useState} from 'react';
+
 
 function App() {
+
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    axios.get('https://buymythingsbackend.onrender.com/')
+    .then((res) => {
+      setData(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }, [])
+
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SEAAAAA
+          <text
+            style={{
+              color: 'red',
+              fontSize: '20px',
+              fontWeight: 'bold',
+            }}
+          >
+            {data}
+          </text>
         </p>
         <a
           className="App-link"
